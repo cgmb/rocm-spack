7b96cd8532a29015eacac813625a5914b36e7b45
Ganesh Kumar <46193450+gkathirv@users.noreply.github.com>
2020-08-26 00:34:26 +0530

    Update package.py

--- var/spack/repos/builtin/packages/rocprim/package.py
+++ var/spack/repos/builtin/packages/rocprim/package.py
@@ -32,11 +32,7 @@ class Rocprim(CMakePackage):
         spec = self.spec
 
         args = [
-            '-DCMAKE_MODULE_PATH={0}/cmake'.format(spec['hip'].prefix),
-            '-DONLY_INSTALL=ON',
-            '-DBUILD_TEST=OFF',
-            '-DBUILD_BENCHMARK=OFF',
-            '-DBUILD_EXAMPLE=OFF'
+            '-DCMAKE_MODULE_PATH={0}/cmake'.format(spec['hip'].prefix)
         ]
 
         return args
